5902abe7e3eee42109356935ee67975990fe9863
Linda <isell.linda@gmail.com>
2020-02-28 11:43:28 +0100

    Set up User model and login and reg

--- server.js
+++ server.js
@@ -6,9 +6,33 @@ import crypto from "crypto"
 import bcrypt from 'bcrypt-nodejs'
 
 //Setting up mongoDB database
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/finalproject-movie"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authMovie"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+    minlength: 2,
+    maxlength: 20
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
